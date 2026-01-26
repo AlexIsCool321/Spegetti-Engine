620465f836fc859304a940192fa722391d3e30a6
Alex <alex.m.diehl1@gmail.com>
2026-01-26 16:46:52 -0600

    Added RenderServer

---
+++ src/Engine/Engine/Headers/Renderer/RenderServer.hpp
@@ -0,0 +1,16 @@
+#ifndef RENDERER_RENDERSERVER_HPP
+#define RENDERER_RENDERSERVER_HPP
+
+#include <System/Window.hpp>
+
+namespace Renderer
+{
+	class RenderServer
+	{
+	public:
+		RenderServer(System::Window* window);
+		virtual ~RenderServer();
+	};
+}
+
+#endif
---
+++ src/Engine/Engine/Sources/Renderer/RenderServer.cpp
@@ -0,0 +1,14 @@
+#include <Renderer/RenderServer.hpp>
+
+namespace Renderer
+{
+	RenderServer::RenderServer(System::Window* window)
+	{
+		
+	}
+
+	RenderServer::~RenderServer()
+	{
+
+	}
+}
