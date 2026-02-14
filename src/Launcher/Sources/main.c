d4f7a5dbda14628cd04c71218d7c60b4758ce1c5
Alex <alex.m.diehl1@gmail.com>
2026-02-14 17:38:23 -0600

    Mesh

--- src/Launcher/Sources/main.c
+++ src/Launcher/Sources/main.c
@@ -20,23 +20,41 @@ int main(int argc, char** argv)
 	void* address = GetWindowProcedureAddress(window);
 	InitRenderer(address);
 
-	const char* vertexShaderSource =
-		"#version 330 core\n"
-		"layout (location = 0) in vec3 aPos;\n"
-		"void main()\n"
-		"{\n"
-		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-		"}\0";
-	
-	const char* fragmentShaderSource =
-		"#version 330 core\n"
-		"out vec4 FragColor;\n"
-		"void main()\n"
-		"{\n"
-		"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-		"}\0";
+	unsigned int shader;
 
-	uint8_t shader = CreateShader(vertexShaderSource, fragmentShaderSource);
+	{
+		const char* vertexShaderSource =
+			"#version 330 core\n"
+			"layout (location = 0) in vec3 aPos;\n"
+			"void main()\n"
+			"{\n"
+			"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+			"}\0";
+		
+		const char* fragmentShaderSource =
+			"#version 330 core\n"
+			"out vec4 FragColor;\n"
+			"void main()\n"
+			"{\n"
+			"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+			"}\0";
+
+		shader = CreateShader(vertexShaderSource, fragmentShaderSource);
+	}
+
+	Vertex vertices[3] =
+	{
+		CreateVertex(Vector3(	-0.5,	-0.5,	 0.0	)),
+		CreateVertex(Vector3(	 0.0,	 0.5,	 0.0	)),
+		CreateVertex(Vector3(	 0.5,	-0.5,	 0.0	))
+	};
+
+	unsigned int indices[3] =
+	{
+		0, 1, 2
+	};
+
+	Mesh* mesh = CreateMesh(vertices, sizeof(vertices)/sizeof(Vertex), indices, sizeof(indices)/sizeof(unsigned int), shader);
 
 	for(;;)
 	{
