b83c68b56a4b7e02e4e2abcacc3488433a37e55c
Alex <alex.m.diehl1@gmail.com>
2026-02-13 22:51:40 -0600

    GL_Renderer

--- src/Launcher/Sources/main.c
+++ src/Launcher/Sources/main.c
@@ -4,6 +4,7 @@
 int main(int argc, char** argv)
 {
 	LoadPlugIn("plugins", "GLFW_Window");
+	LoadPlugIn("plugins", "GL_Renderer");
 
 	InitSystem();
 
---
+++ src/Plugins/GL_Renderer/Headers/GL_Renderer/Instace.h
@@ -0,0 +1,6 @@
+#ifndef GL_RENDERER_INSTANCE_H
+#define GL_RENDERER_INSTANCE_H
+
+void* PLUGIN_CreateInstance(void** pArgs);
+
+#endif
