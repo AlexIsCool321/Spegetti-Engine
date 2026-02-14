9b09232f9d0c413ca292166c41279be55281c83a
Alex <alex.m.diehl1@gmail.com>
2026-02-13 22:18:12 -0600

    Surface

---
+++ src/Engine/Core/Headers/Renderer/Surface.h
@@ -0,0 +1,9 @@
+#ifndef RENDERER_SURFACE_H
+#define RENDERER_SURFACE_H
+
+typedef struct
+{
+	
+} Surface;
+
+#endif
---
+++ src/Plugins/GLFW_Window/Headers/GLFW_Window/Contex.h
@@ -0,0 +1,6 @@
+#ifndef GLFW_WINDOW_CONTEX_H
+#define GLFW_WINDOW_CONTEX_H
+
+void PLUGIN_AttachSurfaceToWindow(void** pArgs);
+
+#endif
