27213a7fd4c841328dd45662319472e7e37294c9
Alex <alex.m.diehl1@gmail.com>
2026-02-13 22:18:12 -0600

    Surface

--- src/Launcher/Sources/main.c
+++ src/Launcher/Sources/main.c
@@ -4,9 +4,10 @@
 int main(int argc, char** argv)
 {
 	LoadPlugIn("plugins", "GLFW_Window");
-	LoadPlugIn("plugins", "GL_GLFW");
 
 	Init_System();
+
+	
 
 	IWindow* window = CreateWindow(800, 600, "Window");
 	CreateWindowContex(window);
--- src/Plugins/GL_GLFW/Headers/GL_GLFW/Contex.h
+++
@@ -1,6 +0,0 @@
-#ifndef GL_GLFW_CONTEX_H
-#define GL_GLFW_CONTEX_H
-
-void PLUGIN_CreateWindowContex(void** args);
-
-#endif
